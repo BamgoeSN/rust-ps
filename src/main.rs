d96911b0656c5ae36a561a7f9fc06c12b051a59f
BamgoeSN <60645387+BamgoeSN@users.noreply.github.com>
2024-02-03 17:34:52 +0900

    Change 4 spaces to a single tab

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,11 @@
 #[allow(unused)]
 use std::{cmp::*, collections::*, iter, mem::*, num::*, ops::*};
 
-fn solve<'t, It: Iterator<Item = &'t str>>(sc: &mut fastio::Tokenizer<It>) {}
+fn solve<'t, It: Iterator<Item = &'t str>>(sc: &mut fastio::Tokenizer<It>) {
+	while let Ok(v) = sc.next_ok::<i32>() {
+		println!("{v}");
+	}
+}
 
 #[allow(unused)]
 mod fastio {
@@ -14,22 +18,32 @@ mod fastio {
 	}
 
 	impl<'i, 's: 'i, It> Tokenizer<It> {
-		pub fn new(text: &'s str, split: impl FnOnce(&'i str) -> It) -> Self { Self { it: split(text) } }
+		pub fn new(text: &'s str, split: impl FnOnce(&'i str) -> It) -> Self {
+			Self { it: split(text) }
+		}
 	}
 
 	impl<'t, It: Iterator<Item = &'t str>> Tokenizer<It> {
-		pub fn next_ok<T: IterParse<'t>>(&mut self) -> PRes<'t, T> { T::parse_from_iter(&mut self.it) }
+		pub fn next_ok<T: IterParse<'t>>(&mut self) -> PRes<'t, T> {
+			T::parse_from_iter(&mut self.it)
+		}
 
-		pub fn next<T: IterParse<'t>>(&mut self) -> T { self.next_ok().unwrap() }
+		pub fn next<T: IterParse<'t>>(&mut self) -> T {
+			self.next_ok().unwrap()
+		}
 
 		pub fn next_map<T: IterParse<'t>, U, const N: usize>(&mut self, f: impl FnMut(T) -> U) -> [U; N] {
 			let x: [T; N] = self.next();
 			x.map(f)
 		}
 
-		pub fn next_it<T: IterParse<'t>>(&mut self) -> impl Iterator<Item = T> + '_ { std::iter::repeat_with(move || self.next_ok().ok()).map_while(|x| x) }
+		pub fn next_it<T: IterParse<'t>>(&mut self) -> impl Iterator<Item = T> + '_ {
+			std::iter::repeat_with(move || self.next_ok().ok()).map_while(|x| x)
+		}
 
-		pub fn next_collect<T: IterParse<'t>, V: std::iter::FromIterator<T>>(&mut self, size: usize) -> V { self.next_it().take(size).collect() }
+		pub fn next_collect<T: IterParse<'t>, V: std::iter::FromIterator<T>>(&mut self, size: usize) -> V {
+			self.next_it().take(size).collect()
+		}
 	}
 }
 
@@ -58,33 +72,42 @@ mod ioutil {
 	}
 
 	impl<'t> Atom<'t> for &'t str {
-		fn parse(text: &'t str) -> PRes<'t, Self> { Ok(text) }
+		fn parse(text: &'t str) -> PRes<'t, Self> {
+			Ok(text)
+		}
 	}
 
 	impl<'t> Atom<'t> for &'t [u8] {
-		fn parse(text: &'t str) -> PRes<'t, Self> { Ok(text.as_bytes()) }
+		fn parse(text: &'t str) -> PRes<'t, Self> {
+			Ok(text.as_bytes())
+		}
 	}
 
 	macro_rules! impl_atom {
-        ($($t:ty) *) => { $(impl Atom<'_> for $t { fn parse(text: &str) -> PRes<Self> { text.parse().map_err(|_| ParseError(text)) } })* };
-    }
+		($($t:ty) *) => { $(impl Atom<'_> for $t { fn parse(text: &str) -> PRes<Self> { text.parse().map_err(|_| ParseError(text)) } })* };
+	}
 	impl_atom!(u8 u16 u32 u64 u128 usize i8 i16 i32 i64 i128 isize f32 f64 bool char String NonZeroI8 NonZeroI16 NonZeroI32 NonZeroI64 NonZeroI128 NonZeroIsize NonZeroU8 NonZeroU16 NonZeroU32 NonZeroU64 NonZeroU128 NonZeroUsize);
 
 	pub trait IterParse<'t>: Sized {
 		fn parse_from_iter<'s, It: Iterator<Item = &'t str>>(it: &'s mut It) -> PRes<'t, Self>
-		where 't: 's;
+		where
+			't: 's;
 	}
 
 	impl<'t, A: Atom<'t>> IterParse<'t> for A {
 		fn parse_from_iter<'s, It: Iterator<Item = &'t str>>(it: &'s mut It) -> PRes<'t, Self>
-		where 't: 's {
+		where
+			't: 's,
+		{
 			it.next().map_or(Err(InputExhaust), <Self as Atom>::parse)
 		}
 	}
 
 	impl<'t, A: IterParse<'t>, const N: usize> IterParse<'t> for [A; N] {
 		fn parse_from_iter<'s, It: Iterator<Item = &'t str>>(it: &'s mut It) -> PRes<'t, Self>
-		where 't: 's {
+		where
+			't: 's,
+		{
 			use std::mem::*;
 			let mut x: [MaybeUninit<A>; N] = unsafe { MaybeUninit::uninit().assume_init() };
 			for p in x.iter_mut() {
@@ -95,9 +118,9 @@ mod ioutil {
 	}
 
 	macro_rules! impl_tuple {
-        ($u:ident) => {};
-        ($u:ident $($t:ident)+) => { impl<'t, $u: IterParse<'t>, $($t: IterParse<'t>),+> IterParse<'t> for ($u, $($t),+) { fn parse_from_iter<'s, It: Iterator<Item = &'t str>>(_it: &'s mut It) -> PRes<'t, Self> where 't: 's { Ok(($u::parse_from_iter(_it)?, $($t::parse_from_iter(_it)?),+)) } } impl_tuple!($($t) +); };
-    }
+		($u:ident) => {};
+		($u:ident $($t:ident)+) => { impl<'t, $u: IterParse<'t>, $($t: IterParse<'t>),+> IterParse<'t> for ($u, $($t),+) { fn parse_from_iter<'s, It: Iterator<Item = &'t str>>(_it: &'s mut It) -> PRes<'t, Self> where 't: 's { Ok(($u::parse_from_iter(_it)?, $($t::parse_from_iter(_it)?),+)) } } impl_tuple!($($t) +); };
+	}
 
 	impl_tuple!(Q W E R T Y U I O P A S D F G H J K L Z X C V B N M);
 }
@@ -126,9 +149,9 @@ use std::io::{BufWriter, StdoutLock};
 static mut WRITER: Option<BufWriter<StdoutLock>> = None;
 #[macro_export]
 macro_rules! print {
-    ($($t:tt)*) => {{ use std::io::*; write!(unsafe{ WRITER.as_mut().unwrap_unchecked() }, $($t)*).unwrap(); }};
+	($($t:tt)*) => {{ use std::io::*; write!(unsafe{ WRITER.as_mut().unwrap_unchecked() }, $($t)*).unwrap(); }};
 }
 #[macro_export]
 macro_rules! println {
-    ($($t:tt)*) => {{ use std::io::*; writeln!(unsafe{ WRITER.as_mut().unwrap_unchecked() }, $($t)*).unwrap(); }};
+	($($t:tt)*) => {{ use std::io::*; writeln!(unsafe{ WRITER.as_mut().unwrap_unchecked() }, $($t)*).unwrap(); }};
 }
